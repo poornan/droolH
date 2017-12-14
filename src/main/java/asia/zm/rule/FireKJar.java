bc40d059e26ad076220415e7f66ea72bdc9d4e39
E Ananthaneshan <poornan@gmail.com>
2017-12-14 14:13:38 +0800

    working

--- src/main/java/asia/zm/rule/FireKJar.java
+++ src/main/java/asia/zm/rule/FireKJar.java
@@ -5,17 +5,19 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
 import asia.zm.dev.coolstore.Order;
+import org.kie.api.runtime.StatelessKieSession;
 
 public class FireKJar {
 	public static void main(String[] args) {
 		KieServices ks = KieServices.Factory.get();
 		KieContainer kContainer = ks.getKieClasspathContainer();
-		KieSession kSession = kContainer.newKieSession("");
+		StatelessKieSession kSession = kContainer.newStatelessKieSession("ksession1");
 
 		Order order = new Order();
 		order.setOrderValue(110D);
-		kSession.insert(order);
-
-		int rules = kSession.fireAllRules();
+		
+		kSession.execute(order);
+        System.out.println(order);
+//		int rules = kSession.fireAllRules();
 	}
 }
