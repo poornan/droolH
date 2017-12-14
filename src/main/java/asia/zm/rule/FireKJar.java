aaf16100809dfc50d64c3eefcacbf6ecfc2f3cb6
E Ananthaneshan <poornan@gmail.com>
2017-12-14 14:42:03 +0800

    working stateless and stateful

--- src/main/java/asia/zm/rule/FireKJar.java
+++ src/main/java/asia/zm/rule/FireKJar.java
@@ -1,5 +1,6 @@
 package asia.zm.rule;
 
+import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -8,16 +9,36 @@ import asia.zm.dev.coolstore.Order;
 import org.kie.api.runtime.StatelessKieSession;
 
 public class FireKJar {
-	public static void main(String[] args) {
-		KieServices ks = KieServices.Factory.get();
-		KieContainer kContainer = ks.getKieClasspathContainer();
-		StatelessKieSession kSession = kContainer.newStatelessKieSession("ksession1");
+    public static void main(String[] args) {
+        runStatefull();
+        runStateless();
+    }
 
-		Order order = new Order();
-		order.setOrderValue(110D);
-		
-		kSession.execute(order);
-        System.out.println(order);
-//		int rules = kSession.fireAllRules();
-	}
+    private static void runStateless() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        StatelessKieSession kSession = kContainer.newStatelessKieSession("ksession1");
+
+        Order order = new Order();
+        order.setOrderValue(110D);
+
+        kSession.execute(order);
+        System.out.println(order.getApprovingOffocer());
+//		int rules = kSession.fireAllRules();ß
+    }
+
+    private static void runStatefull() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieBase base = kContainer.getKieBase("kbase1");
+        KieSession kSession = base.newKieSession();
+
+        Order order = new Order();
+        order.setOrderValue(110D);
+        kSession.insert(order);
+
+        int rules = kSession.fireAllRules();
+        System.out.println(rules);
+        System.out.println(order.getApprovingOffocer());
+    }
 }
